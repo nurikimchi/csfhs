4a27fdcf7c4dae329128b8f6a135d6cf3aaf5d55
Esteban Garcia <estebangarcia2121@gmail.com>
2023-01-19 17:22:56 -0800

    Basic style changes

--- src/lib/components/Container.svelte
+++ src/lib/components/Container.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	export let width: number = 1000;
+  export let width: number = 1000;
 
-	$: widthPixels = `${width}px`;
+  $: widthPixels = `${width}px`;
 </script>
 
 <div class="px-4 h-full">
-	<div class="mx-auto h-full" style:max-width={widthPixels}>
-		<slot />
-	</div>
+  <div class="mx-auto h-full" style:max-width={widthPixels}>
+    <slot />
+  </div>
 </div>
