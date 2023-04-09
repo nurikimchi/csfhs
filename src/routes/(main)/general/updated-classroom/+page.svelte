def0fce421ff06b51a29ada45f731f2fde140d49
Ohmrrr <89240639+Ohmrrr@users.noreply.github.com>
2023-04-09 11:36:02 -0700

    updated classroom page title added

---
+++ src/routes/(main)/general/updated-classroom/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Container from '$lib/components/Container.svelte';
+  import BackdropText from '$lib/components/text/BackdropText.svelte';
+  import { tweened } from 'svelte/motion';
+</script>
+
+<Container>
+  <div>
+    <BackdropText
+      text="Updated Classroom"
+      className="font-extrabold text-5xl md:text-8xl leading-[5rem] md:leading-[8rem]"
+    />
+  </div>
+</Container>
