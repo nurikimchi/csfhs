ff09e33d85ef3c683f91933d64e4dc8e981cac1a
Ohmrrr <89240639+Ohmrrr@users.noreply.github.com>
2023-04-08 22:28:40 -0700

    mobile responsive fix for testimonials

--- src/routes/(main)/courses/[id]/+page.svelte
+++ src/routes/(main)/courses/[id]/+page.svelte
@@ -94,7 +94,7 @@
           className="font-bold text-3xl lg:text-4xl leading-normal sm:leading-[3rem] lg:leading-normal"
         />
 
-        <div class="grid grid-cols-2 lg:grid-cols-3 gap-6 mt-2">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-2">
           {#each course.testimonies as testimony}
             <div>
               <p>{testimony.text}</p>
