e61c0ca3120fdb7555ef2940cf7fb88c658f953d
Esteban Garcia <estebangarcia2121@gmail.com>
2023-04-15 22:24:25 -0700

    Better slide selector

--- src/lib/components/SlideSelector.svelte
+++ src/lib/components/SlideSelector.svelte
@@ -1,26 +1,23 @@
 <script lang="ts">
+  import type { Writable } from 'svelte/store';
+
   type Options = $$Generic<string[] | readonly string[]>;
   type Option = Options[number];
 
   export let options: Options;
-  export let onClick: (option: Option) => void;
+  export let selected: Writable<Option>;
 
   const selectedClasses = 'bg-blue-600 text-white';
-
-  let selected: Option = options[0];
 </script>
 
 <div class="rounded-md flex flex-row bg-gray-50 overflow-hidden my-2 shadow-inner">
   {#each options as option}
     <button
       class="flex-1 py-2 text-xs capitalize transition rounded-md font-medium tracking-wide {option ===
-      selected
+      $selected
         ? selectedClasses
         : 'hover:text-blue-600'}"
-      on:click={() => {
-        selected = option;
-        onClick(option);
-      }}
+      on:click={() => ($selected = option)}
     >
       {option}
     </button>
