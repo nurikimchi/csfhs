1dee01504a39c87519a33163ac12f3c9c050d689
estebangarcia21 <estebangarcia2121@gmail.com>
2023-12-02 18:20:05 -0800

    Remove unused supabase dependencies

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -1,21 +1,6 @@
 <script lang="ts">
-  import { supabase } from '$lib/supabaseClient';
-  import { invalidate } from '$app/navigation';
-  import { onMount } from 'svelte';
-  import Navbar from '$lib/components/navbar/Navbar.svelte';
   import Footer from '$lib/components/Footer.svelte';
-
-  onMount(() => {
-    const {
-      data: { subscription }
-    } = supabase.auth.onAuthStateChange(() => {
-      invalidate('supabase:auth');
-    });
-
-    return () => {
-      subscription.unsubscribe();
-    };
-  });
+  import Navbar from '$lib/components/navbar/Navbar.svelte';
 </script>
 
 <svelte:head>
