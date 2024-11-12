626398e6fe1af5da0a342dbfb3ad9fbb800e7cf4
nurikimchi <nurikimchi@gmail.com>
2024-11-11 23:09:03 -0800

    Remove default export by adding bracket

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import { adapter } from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { VERSION } from '@sveltejs/kit';
 
