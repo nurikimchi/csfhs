38a46e1caa9ab756aecda2d1e25ce91cb96f26fa
nurikimchi <nurikimchi@gmail.com>
2024-11-11 23:06:31 -0800

    Remove VERSION use

--- svelte.config.js
+++ svelte.config.js
@@ -2,12 +2,13 @@ import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { VERSION } from '@sveltejs/kit';
 
+console.log("the version of @sveltejs/kit" + VERSION);
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
-		version: VERSION
 	}
 };
 
