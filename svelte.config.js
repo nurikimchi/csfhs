ed493c3439a3d4c8a28c93d71e9a7a3fed157e7f
nurikimchi <nurikimchi@gmail.com>
2024-12-08 22:10:54 -0800

    Implement deployment fixes

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter()
-	}
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter()
+  }
 };
 
 export default config;
