0616d6e056148072fc654817873f3ed6a539fddc
nurikimchi <nurikimchi@gmail.com>
2025-01-08 02:50:34 -0800

    Update title to full version of abbrev.

--- src/lib/components/TeacherCard.svelte
+++ src/lib/components/TeacherCard.svelte
@@ -10,7 +10,7 @@
   export const TEACHER_OPTIONS: Record<Teacher, Options> = {
     'Mr. Dagler': {
       name: 'Clay Dagler',
-      title: 'AP CSA, ECS, MLH, and CS&R Instructor',
+      title: 'CS & Robotics for Beginners | Exploring Computer Science | AP Computer Science A | Machine Learning Honors',
       description:
         "As the head of the Computer Science pathway here at Franklin High, I hope to encourage my students to pursue a career in computer science. I started out teaching Math after graduating from the University of California, Davis, but slowly moved towards robotics and computer science, where I found engaged students learning more about mathematics. I've since worked as a CTE teacher with Franklin, finding any opportunities for my students to involve themselves with technology.",
       image: DaglerPFP,
@@ -18,7 +18,7 @@
     },
     'Mrs. Rodriguez': {
       name: 'Lynette Rodriguez',
-      title: 'AP CSP and Web Development Instructor',
+      title: 'AP Computer Science Principles',
       description: '',
       image: RodriguezPFP,
       email: 'lrrodrig@egusd.net@egusd.net'
@@ -44,7 +44,7 @@
   $: ({ name, image, title, description, email } = getOptions(teacher));
 </script>
 
-<a href="mailto:{email}" class="rounded-md block p-8 transition shadow border">
+<a href="mailto:{email}" class="rounded-md block p-8 transition border">
   <img src={image} alt={name} class="rounded-full w-24 mx-auto" />
   <div class="text-center mt-4">
     <h1 class="font-body text-xl font-semibold">{name}</h1>
