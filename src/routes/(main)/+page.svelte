408c90e3a78c6e232f975f8756c4fa0551e119fe
nurikimchi <nurikimchi@gmail.com>
2025-01-08 03:04:57 -0800

    Fix alumni institution images

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -10,6 +10,12 @@
   import CsLogo from '$lib/images/csfhs.jpg';
   import Testimonials  from '$lib/utils/testimonials'
 
+  import StanfordSvg from '$lib/images/stanford.svg';
+  import BerkeleySvg from '$lib/images/berkeley.svg';
+  import CarnegieMellonSvg from '$lib/images/carnegie-mellon.svg';
+  import GoogleSvg from '$lib/images/google.svg';
+  import UclaSvg from '$lib/images/ucla.svg'
+
 
   export let data: PageData;
 </script>
@@ -108,11 +114,11 @@
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <h2 class="text-center text-lg/8 font-semibold text-gray-900">Graduates went on to:</h2>
     <div class="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-      <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="src/lib/images/stanford.svg" alt="Transistor" width="158" height="48">
-      <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="src/lib/images/berkeley.svg" alt="Reform" width="158" height="48">
-      <img class="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1" src="src/lib/images/google.svg" alt="SavvyCal" width="158" height="48">
-      <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="src/lib/images/ucla.svg" alt="Tuple" width="158" height="48">
-      <img class="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1" src="src/lib/images/carnegie-mellon.svg" alt="Statamic" width="158" height="48">
+      <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src={StanfordSvg} alt="Transistor" width="158" height="48">
+      <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src={BerkeleySvg} alt="Reform" width="158" height="48">
+      <img class="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1" src={GoogleSvg} alt="SavvyCal" width="158" height="48">
+      <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src={CarnegieMellonSvg} alt="Tuple" width="158" height="48">
+      <img class="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1" src={UclaSvg} alt="Statamic" width="158" height="48">
     </div>
   </div>
 </div>
