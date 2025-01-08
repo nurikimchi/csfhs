cb75e30202eb679f2bab47d4167111e40b09da1b
nurikimchi <nurikimchi@gmail.com>
2025-01-08 02:46:29 -0800

    Add testimonials for homepage

--- src/lib/utils/testimonials.ts
+++ src/lib/utils/testimonials.ts
@@ -1,13 +1,15 @@
 export interface Testimonials {
     name: string;
+    subtitle: string;
     quote: string;
     class: string;
     id: number;
   }
   
-  const testimonials: Testimonials[] = [
+  const Testimonials: Testimonials[] = [
     {
       name: 'Justin Gagne',
+      subtitle: '',
       quote:
         'I enjoyed collaborating with others in the lessons and labs. Despite the 3 years of prior programming experience we had, the class is set up in a way that would allow anyone without experience to succeed just as well. The labs we do are formatted in a way that mirrors the lessons we have just learned, making it significantly easier to grasp the complex concepts taught in this class. I also enjoyed learning about the different ways we could use machines to predict data and which ones work better in certain situations. Additionally, we learn how it is applied to real life, something that other classes like math fail to highlight.',
       class:
@@ -16,6 +18,7 @@ export interface Testimonials {
     },
     {
       name: 'Sayan Patel',
+      subtitle: '',
       quote:
         'I really like the way the class is organized, and how the students are able to figure out how to make their code work using the help of other students. The lessons are straight-forward and made perfectly for students to learn, and the labs are really helpful for understanding how to implement different things into code. And most of all, the teacher is the best.',
       class:
@@ -23,19 +26,30 @@ export interface Testimonials {
       id: 2,
     },
     {
-      name: 'Industry Connections',
+      name: 'Joshua Fong',
+      subtitle: '',
       quote:
-        'Industry connections that help shape the pathway, and give vast opportunities to students looking to go beyond the classroom.',
+        'Taking Exploring Computer Science and the CS pathway has helped me learn that programming is a team effort and not just something to tackle myself. Talking problems out in a classroom designed for collaboration gets me excited for the engaging experiences I’ll have in college.',
       class:
-        'We believe that in order for students to receive the best education, they need to go beyond the traditional classroom. All of the offered courses in the CS pathway include guest speakers, field trips, and programming competitions. These activities help students discover a possible career path and develop connections with prominent figures in the industry. Our business and community partners are:',
+        'Exploring Computer Science',
       id: 3,
     },
     {
-      name: 'Student Leadership',
+      name: 'Ankur',
+      subtitle: 'Software Engineer at Google',
       quote:
-        'Another opportunity for students to participate in the pathway outside of the classroom is by being a part of the leadership team',
+        'Franklin gave me the foundation to take my skills to the next level and land a great job. It’s cool to see how the program continues to build opportunities for others.',
       class:
-        'Another way students can go beyond the classroom is by participating in our student leadership team. As a leader, students will work with their team to promote computer science education and recruit future students for the pathway.',
+        '',
+      id: 4,
+    },
+    {
+      name: 'Marlon',
+      subtitle: 'Associate IT Security Engineer @Stanford Healthcare',
+      quote:
+        'My interest for software and cybersecurity flourished under Mr. Dagler’s tutelage at Franklin. I’m happy to be in this field and hope that Franklin inspires more computer scientists to keep going!',
+      class:
+        '',
       id: 4,
     }
   ];
@@ -50,4 +64,4 @@ export interface Testimonials {
 //     return getMoreInfoId(infoPageId);
 //   }
   
-export default testimonials;
+export default Testimonials;
